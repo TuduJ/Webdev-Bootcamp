2d8c8cc564c5959eb17ab9acc8e0af2f00e81df8
unknown <2018pgcaca34@nitjsr.ac.in>
2020-04-01 20:04:39 +0530

    factorial updated

--- Javascript/Functions/factorial.js
+++ Javascript/Functions/factorial.js
@@ -1,6 +1,28 @@
-function fact(n){
-    if(n === 0)
+// Using recursion
+function fact_r(num){
+    if(num === 0)
         return 1;
     else
-        return (n * fact(n-1));
+        return (num * fact(num-1));
 }
+
+// Using For loop
+// function fact_f(num){
+//     var fact = 1;
+//     if(num === 0)
+//         return fact;
+//     else{
+//         for(var i = 1 ; i<=num ; i++)
+//             fact = fact * i;
+//         return fact;
+//     }
+// }
+
+// Using While loop
+// function fact_w(num){
+//     var fact = 1;
+//     while (num !== 0){
+//         fact *= num;
+//         num--;
+//     }
+// }
