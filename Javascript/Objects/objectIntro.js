1499ac2443d016240f7e5950826ea3a1d528e179
unknown <2018pgcaca34@nitjsr.ac.in>
2020-04-09 16:02:28 +0530

    objects

---
+++ Javascript/Objects/objectIntro.js
@@ -0,0 +1,23 @@
+//  ---------------Syntax----------------- 
+//     var <object_name> = {
+//         key1 = value1,
+//         key2 = value2,
+//         .
+//         .
+//         .
+//         .
+//         keyn = valuen
+//     };
+
+var person = {
+    name : "Travis",
+    age : 21,
+    city : "LA"
+};
+
+
+// -------------Retrieving Data--------------- 
+//Bracket notation
+console.log(person["name"]);
+//Dot notation
+console.log(person.age);
