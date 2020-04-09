d72a500e8f384bdc5ab86d595092777e1794448f
unknown <2018pgcaca34@nitjsr.ac.in>
2020-04-09 22:02:32 +0530

    objects retrieving property

--- Javascript/Objects/objectIntro.js
+++ Javascript/Objects/objectIntro.js
@@ -21,3 +21,17 @@ var person = {
 console.log(person["name"]);
 //Dot notation
 console.log(person.age);
+
+
+// We cannot use dot notation if the property starts with a number
+someObject.1property    //INVALID
+someObject["1property"] //VALID
+
+//We cannot lookup using a variable with bracket notation
+var str = "str"
+someObject.str      //Doesn't look for "name"
+someObject["str"]   //Does evaluate str and looks for "name"
+
+// We cannot use dot notation for the property names with spaces
+someObject.fav Color    //INVALID
+someObject["fav color"] //VALID
