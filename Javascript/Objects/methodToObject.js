71d2f51dddc3deee99fb4535a7fc165fe2834e9f
unknown <2018pgcaca34@nitjsr.ac.in>
2020-04-10 17:07:46 +0530

    object methods

---
+++ Javascript/Objects/methodToObject.js
@@ -0,0 +1,13 @@
+var obj = {
+    name: "Jitendra",
+    age: 26,
+    isCool: true,
+    friends: ["Bikas", "Ankur", "John", "Sarwna"],
+    
+    //prototype of function add 
+    add: function(x,y){             //add is the function name
+        return x+y;                 //function definition
+    }
+}
+
+// 
