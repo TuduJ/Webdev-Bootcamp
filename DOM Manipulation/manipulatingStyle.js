1610ba80c13b4628736c4986036f1614a7ac8240
unknown <2018pgcaca34@nitjsr.ac.in>
2020-04-11 19:37:28 +0530

    DOM style

---
+++ DOM Manipulation/manipulatingStyle.js
@@ -0,0 +1,26 @@
+//      DOM Manipulation
+// ----------Topics to be covered-----------
+// 1. Changing an Element
+// 2. Adding/removing classes
+// 3. Changing the content of a tag
+// 4. Changing attributes(src,href,etc.)
+
+
+// STYLE Property
+// select
+var tag = document.getElementById("top");
+// Manipulate
+tag.style.color = "pink";
+tag.style.border = "10px solid red";
+tag.style.fontSize = "70px";
+tag.style.background = "yellow";
+tag.style.marginTop = "200px";
+
+// Alternate way of styling is
+// some class id defined in the css file.
+// tag.classList.add("someClass");          <-------Alternate way
+
+// classList method
+// 1. tag.classList.add("clase_name");
+// 2. tag.classList.remove("clase_name");
+// 3. tag.classList.toggle("clase_name");       <--- If the element has the 'class_name' class then it will remove it other wise it will add it to the element.
