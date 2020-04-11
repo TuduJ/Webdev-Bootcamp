6fa348a3261bf47889f8573b696e85931ddcedc6
unknown <2018pgcaca34@nitjsr.ac.in>
2020-04-11 14:04:53 +0530

    selector methods

---
+++ DOM Manipulation/selectorMethods.js
@@ -0,0 +1,29 @@
+// ------ Important Selector Methods -------
+//      1. documment.getElementById()
+//      2. document.getElementsByClassName()
+//      3. document.getElementsByTagName()
+//      4. document.querySelector()
+//      5. document.querySelectorAll()
+
+//      console.dir(tag) ---> shows the DOM of the tag
+
+
+
+// --------- getElementByID -----------
+var tag = document.getElementById("highlight");
+// call tag in the console of the browser
+
+
+// --------- getElementsByClassName -----------
+var tags = document.getElementsByClassName("bolder");
+// call tag in the console of the browser
+
+
+// --------- getElementsByTagName -----------
+var tags = document.getElementsByTagName("li");
+// call tag in the console of the browser
+
+
+// ---------- querySelector -----------
+var tag = document.querySelector("h1");
+var li = document.querySelector("li a.special");
