0947430ec32ee391067dd8fe7a7175158780ea09
unknown <2018pgcaca34@nitjsr.ac.in>
2020-04-15 13:43:38 +0530

    toggle color exercise

---
+++ DOM Events/toggleColorExercise.js
@@ -0,0 +1,16 @@
+var button = document.querySelector("button");
+var body = document.querySelector("body");
+var isPurple = false;
+function toggleColor(){
+    if(isPurple){
+        body.style.background = "white";
+        // isPurple = false;
+    }
+    else{
+        body.style.background = "purple";
+        // isPurple = true;
+    }
+    isPurple = !isPurple;                   // <----- instead of changing color at line 7 and 11 we can use this in one line. Every time the clor changes the boolean value also toggles.
+}
+
+button.addEventListener("click", toggleColor);
