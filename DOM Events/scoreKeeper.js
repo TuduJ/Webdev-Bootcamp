d90b3576f765f984a27ce2085b0418f8d4a999e3
unknown <2018pgcaca34@nitjsr.ac.in>
2020-04-16 00:05:15 +0530

    scoreKeeper updated

--- DOM Events/scoreKeeper.js
+++ DOM Events/scoreKeeper.js
@@ -1,25 +1,28 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.querySelector("#p2");
-var reset = document.querySelector("#reset");
+var resetButton = document.querySelector("#reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
-var maxScore = document.querySelector("#maxScore");
+var maxScoreDisplay = document.querySelector("#maxScoreDisplay");
 var inputMaxScore = document.querySelector("#inputMaxScore");
+// var numInput = document.querySelector("input");
+
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
 
-// maxScore.textContent = inputMaxScore;
-maxScore = 5;
+// maxScoreDisplay.textContent = inputMaxScore;
+var winScore = 5;
 
 p1Button.addEventListener("click", function(){
     if(!gameOver){
         p1Score++;
         p1Display.textContent = p1Score;
-        if(p1Score === maxScore){
-            gameOver = true;
-            p1Display.style.color = "green";
-            p2Display.style.color = "red";
+        // console.log(p1Score, winScore);
+        if(p1Score === winScore){
+            gameOver = ! gameOver;
+            p1Display.classList.add("winner");
+            p2Display.classList.add("wellPlayed");
         }
     }
 });
@@ -28,21 +31,36 @@ p2Button.addEventListener("click", function(){
     if(!gameOver){
         p2Score++;
         p2Display.textContent = p2Score;
-        if(p2Score === maxScore){
-            gameOver = true;
-            p2Display.style.color = "green";
-            p1Display.style.color = "red";
+        if(p2Score === winScore){
+            gameOver = !gameOver;
+            p2Display.classList.add("winner");
+            p1Display.classList.add("wellPlayed");
         }
     }
 });
 
-reset.addEventListener("click", function(){
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
-    p1Display.style.color = "black";
-    p2Display.style.color = "black";
+function reset(){
     p1Score = 0;
     p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner","wellPlayed");
+    p2Display.classList.remove("winner","wellPlayed");
     gameOver = false;
+    winScore = 5;
+}
 
+resetButton.addEventListener("click", function(){
+    reset();
+    inputMaxScore.value = null;
+    maxScoreDisplay.textContent = winScore;
 });
+
+inputMaxScore.addEventListener("change",function(){
+    reset();
+    maxScoreDisplay.textContent = inputMaxScore.value;
+    winScore = Number(inputMaxScore.value);
+});
+// numInput.addEventListener("change",function(){
+//     alert("Clicked the input")
+// });
