578af6cca8410f302a51842e228dbcb865729267
unknown <2018pgcaca34@nitjsr.ac.in>
2020-04-15 13:25:53 +0530

    click event listener

--- DOM Events/EventsIntroduction.js
+++ DOM Events/EventsIntroduction.js
@@ -2,14 +2,42 @@
 // <----- SYNTAX ------>
 // element.addEventListener(type,functionToCall);
 
-var h1 = document.querySelector("h1");
 
-h1.addEventListener("click",function(){
-    alert("H1 was clicked");
-})
-
-h1.addEventListener("click",function(){
-    h1.style.background = "orange";
-})
+// Example:-
+// var h1 = document.querySelector("h1");
+// h1.addEventListener("click",function(){              <---- Event listener 1
+//     alert("H1 was clicked");
+// })
+// h1.addEventListener("click",function(){              <----- Event Listener 2
+//     h1.style.background = "orange";
+// })
 
 // We can add more than one listener on an element, on the same/different event type.
+
+
+
+// Examle to show changing of ul color everytime it is clicked
+
+// var lis = document.querySelectorAll("li");
+
+// for(var i = 0; i < lis.length; i++){
+//     lis[i].addEventListener("click",function(){
+//         if(this.style.color == "red"){
+//             this.style.color = "black";
+//     }
+//         else{
+//             this.style.color = "red";
+// }
+// });
+// }
+
+
+// Another example for event listener
+var button = document.querySelector("button");
+var paragraph = document.querySelector("p");
+
+function changeText(){
+    paragraph.textContent = "You clicked the button!!";
+}
+
+button.addEventListener("click", changeText);
