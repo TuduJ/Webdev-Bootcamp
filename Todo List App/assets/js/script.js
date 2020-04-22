fddcd3233d89643915108d12da7f600b929eaaba
unknown <jitendratd@gmail.com>
2020-04-23 01:25:15 +0530

    toto list completed

--- Todo List App/assets/js/script.js
+++ Todo List App/assets/js/script.js
@@ -1,5 +1,26 @@
-var list_colors = 
+
 // Check of specific todo by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        // console.log($(this).val());
+        var todoText = $(this).val();
+        $("ul").append("<li><span><i class='fas fa-trash-alt'></i> </span>" + todoText + "</li>");
+        $(this).val(null);
+    }
+});
+
+$(".fa-plus-square").click(function(){
+    $("input[type='text']").fadeToggle();
 });
